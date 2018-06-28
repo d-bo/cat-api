0202b0d0f310d3062cdebb1a4266b9f0fe3c0670
d-bo <bolhovitinov_d@goldapple.ru>
2018-06-28 13:07:21 +0500

    openshift test app.py

--- app.py
+++ app.py
@@ -2012,4 +2012,4 @@ if __name__ == "__main__":
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     port = int(os.environ.get('PORT', 5000)) # Heroku check port
-    app.run(host='0.0.0.0', threaded=True, debug=True, port=port)
+    app.run()
